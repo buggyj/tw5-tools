ed6b9672addc71602016a6fe9e0602c717e868ae
buggyj <buggyjef@gmail.com>
2017-10-14 19:21:21 +0200

    basewidget: defaults now only loaded when widget is compiled

--- xplugins/bj/basewidget/basewidget.js
+++ xplugins/bj/basewidget/basewidget.js
@@ -17,11 +17,21 @@ var Widget = require("$:/core/modules/widgets/widget.js").widget;
 var baseWidget = function(parseTreeNode,options) {
 	this.initialise(parseTreeNode,options);
 };
-
 /*
 Inherit from the base widget class
 */
 baseWidget.prototype = new Widget();
+/*
+set the defaults (they were provided in the api tid)
+*/
+baseWidget.prototype.initialise = function(parseTreeNode,options) {
+	Widget.prototype.initialise.call(this,parseTreeNode,options);
+	this.setdefaults();
+}
+/*
+expose the widgets default parameters
+*/
+baseWidget.prototype.defaults = [];
 
 baseWidget.prototype.setvars = function(defaults) {
 	var self = this;
@@ -29,7 +39,7 @@ baseWidget.prototype.setvars = function(defaults) {
 		if (!!vari["default"]) self.attributes[vari.name]=vari["default"];
 	});
 }
-
+	
 baseWidget.prototype.setdefaults = function() {
 	this.setvars(this.defaults);
 }
